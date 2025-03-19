61e0210305ace6d436f2dbe4b5fe5616bef4adb8
mewilker <mewilk@byu.edu>
2025-03-18 19:39:47 -0600

    Stub an SQL DAO so the other branch can verify changes to it

---
+++ server/src/main/java/dataaccess/SQLUserDAO.java
@@ -0,0 +1,20 @@
+package dataaccess;
+
+import datamodels.UserData;
+
+public class SQLUserDAO implements UserDAO{
+    @Override
+    public void insertUser(UserData user) throws DataAccessException {
+
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+
+    }
+}
