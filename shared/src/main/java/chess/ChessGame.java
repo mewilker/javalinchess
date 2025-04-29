bf70d63ba79d988e03600ee700fa85cf41d16cfe
mewilker <mewilk@byu.edu>
2025-04-29 17:03:53 -0600

    Equals and Hashcode for ChessGame.java

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -2,6 +2,7 @@ package chess;
 
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Objects;
 
 /**
  * For a class that can manage a chess game, making moves on a board
@@ -186,5 +187,18 @@ public class ChessGame {
      */
     public ChessBoard getBoard() {
         return board;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessGame chessGame = (ChessGame) o;
+        return Objects.equals(board, chessGame.board) && currTurn == chessGame.currTurn;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(board, currTurn);
     }
 }
