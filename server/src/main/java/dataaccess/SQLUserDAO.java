38f06397b304690d50d1ac4dfde2f1d588c160d2
mewilker <mewilk@byu.edu>
2025-04-30 08:25:34 -0600

    update SQLUserDAO to work

--- server/src/main/java/dataaccess/SQLUserDAO.java
+++ server/src/main/java/dataaccess/SQLUserDAO.java
@@ -4,12 +4,12 @@ import datamodels.UserData;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Arrays;
 
-public class SQLUserDAO implements UserDAO{
+public class SQLUserDAO implements UserDAO {
 
-    public SQLUserDAO() throws DataAccessException{
+    public SQLUserDAO() throws DataAccessException {
         String statement = """
                 CREATE TABLE  IF NOT EXISTS users (
                     username VARCHAR(255) NOT NULL,
@@ -23,23 +23,53 @@ public class SQLUserDAO implements UserDAO{
             preparedStatement.executeUpdate();
         }
         catch (SQLException e){
-            System.err.println(Arrays.toString(e.getStackTrace()));
-            throw new DataAccessException(e.getMessage());
+            throw new DataAccessException("Could not create users table", e);
         }
     }
 
     @Override
     public void insertUser(UserData user) throws DataAccessException {
-
+        String statement = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
+        try (Connection connection = DatabaseManager.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(statement)){
+            preparedStatement.setString(1, user.username());
+            preparedStatement.setString(2, user.password());
+            preparedStatement.setString(3, user.email());
+            preparedStatement.executeUpdate();
+        }
+        catch (SQLException e){
+            throw new DataAccessException("Could not insert into users", e);
+        }
     }
 
     @Override
     public UserData getUser(String username) throws DataAccessException {
+        String statement = "SELECT * FROM users WHERE username = ?";
+        try (Connection connection = DatabaseManager.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(statement)){
+            preparedStatement.setString(1, username);
+            try(ResultSet resultSet = preparedStatement.executeQuery()){
+                if (resultSet.next()){
+                    return new UserData(resultSet.getString("username"),
+                            resultSet.getString("password"), resultSet.getString("email"));
+                }
+            }
+        }
+        catch (SQLException e){
+            throw new DataAccessException("Error getting user", e);
+        }
         return null;
     }
 
     @Override
     public void clear() throws DataAccessException {
-
+        String statement = "TRUNCATE users";
+        try(Connection connection = DatabaseManager.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(statement)) {
+            preparedStatement.executeUpdate();
+        }
+        catch (SQLException e){
+            throw new DataAccessException("Could not clear users", e);
+        }
     }
 }
