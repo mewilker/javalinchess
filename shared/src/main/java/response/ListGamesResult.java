81cd0b816ea2897f8c2e38dc9f30450f1aeb50ba
mewilker <mewilk@byu.edu>
2025-05-01 11:29:32 -0600

    Added a getgames method

--- shared/src/main/java/response/ListGamesResult.java
+++ shared/src/main/java/response/ListGamesResult.java
@@ -9,4 +9,8 @@ public class ListGamesResult extends Result{
     public ListGamesResult(ArrayList<GameData> games){
         this.games = games;
     }
+
+    public ArrayList<GameData> getGames(){
+        return games;
+    }
 }
