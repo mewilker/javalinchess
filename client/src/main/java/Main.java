20204df18fb9fe11f3828b32ff9d1f79cb209edd
mewilker <mewilk@byu.edu>
2025-05-02 14:14:07 -0600

    Create structure for ui and logic that controls ui

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,13 @@
 import chess.*;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
+        try(Scanner s = new Scanner(System.in)){
+
+        }
     }
 }
---
+++ client/src/main/java/ui/Display.java
@@ -0,0 +1,10 @@
+package ui;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Display {
+    private PrintStream out;
+    private Scanner in;
+
+}
