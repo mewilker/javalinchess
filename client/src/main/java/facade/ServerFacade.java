53d02f6704df37960fb1a9a7957aeababb8e3ed3
mewilker <mewilk@byu.edu>
2025-05-02 13:15:05 -0600

    add register, login, logout, createGame,listGames

--- client/src/main/java/facade/ServerFacade.java
+++ client/src/main/java/facade/ServerFacade.java
@@ -1,35 +1,83 @@
 package facade;
 
+import datamodels.GameData;
 import requests.*;
 import response.*;
 
+import java.util.ArrayList;
+
 public class ServerFacade {
     private final String url;
+    private final HttpCommunicator http = new HttpCommunicator();
+
+    private String authToken = "";
+
     public ServerFacade(String url){
         this.url = url;
     }
 
-    public Result register(RegisterRequest request){
-        return null;
+    public void register(String username, String password, String email) throws ServerErrorException {
+        RegisterRequest request = new RegisterRequest(username, password, email);
+        Result result = http.doPost(url + "/user", authToken, request);
+        if (result instanceof LoginResult login){
+            authToken = login.getAuthToken();
+        }
+        else{
+            //TODO: log to standard err
+            if (http.getLastStatusCode() == 403){
+                throw new ServerErrorException("Username already taken");
+            }
+            throw new ServerErrorException(result.getMessage());
+        }
     }
 
-    public Result login(LoginRequest request){
-        return null;
+    public void login(String username, String password) throws ServerErrorException{
+        LoginRequest request = new LoginRequest(username, password);
+        Result result = http.doPost(url + "/session", authToken, request);
+        if (result instanceof LoginResult login){
+            authToken = login.getAuthToken();
+        }
+        else{
+            //TODO: log to standard err
+            if (http.getLastStatusCode() == 401){
+                throw new ServerErrorException("Username or password was incorrect");
+            }
+            throw new ServerErrorException(result.getMessage());
+        }
     }
 
-    public Result logout(LogoutRequest request){
-        return null;
+    public void logout() throws ServerErrorException {
+        Result result = http.doDelete(url + "/session", authToken);
+        authToken = "";
+        if (http.getLastStatusCode() != 200){
+            //TODO: log to standard err
+            throw new ServerErrorException("Logout was unsuccessful");
+        }
     }
 
-    public Result createGame(CreateGameRequest request){
-        return null;
+    public int createGame(String name) throws ServerErrorException{
+        CreateGameRequest request = new CreateGameRequest(name, "");
+        Result result = http.doPost(url + "/game", authToken, request);
+        if (result instanceof CreateGameResult create){
+            return create.getGameID();
+        }
+        //TODO: log to standard err
+        if(http.getLastStatusCode() == 401){
+            authToken = "";
+            throw new ServerErrorException("Authorization expired");
+        }
+        throw new ServerErrorException(result.getMessage());
     }
 
-    public Result listGames(){
-        return null;
+    public ArrayList<GameData> listGames() throws ServerErrorException{
+        Result result = http.doGet(url + "/game", authToken);
+        if (result instanceof ListGamesResult list){
+            return list.getGames();
+        }
+        throw new ServerErrorException(result.getMessage());
     }
 
-    public Result joinGame(JoinGameRequest request){
+    public Result joinGame(JoinGameRequest request) throws ServerErrorException{
         return null;
     }
 }
