672bc07226bf8eeeb6d0eddf030d4699d1e9a64a
mewilker <mewilk@byu.edu>
2025-05-02 13:15:40 -0600

    create ServerErrorException

---
+++ client/src/main/java/facade/ServerErrorException.java
@@ -0,0 +1,11 @@
+package facade;
+
+public class ServerErrorException extends Exception{
+    public ServerErrorException(String message){
+        super(message);
+    }
+
+    public ServerErrorException(String message, Throwable ex){
+        super(message, ex);
+    }
+}
