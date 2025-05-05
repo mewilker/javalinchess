df119d8ddd3b1d54c56a693113e1cd33046deffe
mewilker <mewilk@byu.edu>
2025-05-05 14:40:06 -0600

    ConsoleDisplay now manages an in and an out stream, along with title and entryField elements

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,4 +1,5 @@
 import chess.*;
+import ui.ConsoleDisplay;
 
 import java.util.Scanner;
 
@@ -7,6 +8,8 @@ public class Main {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
         try(Scanner s = new Scanner(System.in)){
+            ConsoleDisplay display = new ConsoleDisplay(System.out, s);
+            display.title();
 
         }
     }
---
+++ client/src/main/java/ui/ConsoleDisplay.java
@@ -0,0 +1,56 @@
+package ui;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+import static ui.EscapeSequences.*;
+
+public class ConsoleDisplay {
+    private final PrintStream out;
+    private final Scanner in;
+
+    public ConsoleDisplay(PrintStream out, Scanner in){
+        this.out = out;
+        this.in = in;
+    }
+
+    public void title(){
+        out.print(SET_BG_COLOR_BLACK);
+        out.print(SET_TEXT_COLOR_GREEN);
+        out.print(SET_TEXT_BOLD);
+        out.print(WHITE_QUEEN);
+        out.print(" Welcome to Makenna's Chess Server!");
+        out.print(WHITE_QUEEN);
+        out.print("\n\n\n");
+        out.print(RESET_TEXT_BOLD_FAINT);
+        out.print(SET_TEXT_COLOR_BLUE);
+    }
+
+    public String getInput(){
+        out.print(SET_TEXT_COLOR_WHITE);
+        String line;
+        line = in.nextLine();
+        out.print(SET_TEXT_COLOR_BLUE);
+        return line;
+
+    }
+
+    public String entryField(String prompt){
+        out.print(SET_TEXT_BOLD);
+        out.print(prompt + ":");
+        out.print(RESET_TEXT_BOLD_FAINT);
+        String result = getInput();
+        out.print(SET_TEXT_COLOR_BLUE);
+        return result;
+    }
+
+    //Seems a bit funny, but I want display to keep track of the print stream, not the context.
+    //Context may need to keep track of specific Messages to display to the user tho
+    public void printText(String toPrint){
+        out.print(RESET_TEXT_BOLD_FAINT);
+        out.print(RESET_TEXT_ITALIC);
+        out.print(SET_TEXT_COLOR_BLUE);
+        out.print(toPrint);
+    }
+
+}
