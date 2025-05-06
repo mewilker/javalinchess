9ea4a042ad652efdea1d75d241b0f77f23730cfe
mewilker <mewilk@byu.edu>
2025-05-06 11:01:35 -0600

    logout and work on gameData structure

--- client/src/main/java/context/PostLogin.java
+++ client/src/main/java/context/PostLogin.java
@@ -1,11 +1,18 @@
 package context;
 
+import datamodels.GameData;
+import facade.ServerErrorException;
 import facade.ServerFacade;
 import ui.Display;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class PostLogin implements Context{
     ServerFacade server;
     Display display;
+    int nextID = 1;
+    HashMap<Integer, GameData> gameStorage;
 
     public PostLogin(ServerFacade server, Display display){
         this.server = server;
@@ -34,6 +41,22 @@ public class PostLogin implements Context{
     }
 
     private Context logout(){
+        try {
+            server.logout();
+        }
+        catch (ServerErrorException e){
+            display.printError(e.getMessage());
+        }
         return new PreLogin(display, server);
+    }
+
+    private void populateGames() throws ServerErrorException{
+        ArrayList<GameData> games = server.listGames();
+        int index = 1;
+        for(GameData game : games){
+            gameStorage.put(index, game);
+            index++;
+        }
+        nextID = index;
     }
 }
