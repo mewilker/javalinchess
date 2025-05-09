b44c50c8afc508c705bfb3ea91f4e1a45e5fc054
mewilker <mewilk@byu.edu>
2025-05-08 19:17:02 -0600

    toString uses gson to serialize

--- shared/src/main/java/websocket/commands/UserGameCommand.java
+++ shared/src/main/java/websocket/commands/UserGameCommand.java
@@ -1,5 +1,7 @@
 package websocket.commands;
 
+import com.google.gson.Gson;
+
 import java.util.Objects;
 
 /**
@@ -39,6 +41,11 @@ public class UserGameCommand {
 
     public Integer getGameID() {
         return gameID;
+    }
+
+    @Override
+    public String toString(){
+        return new Gson().toJson(this);
     }
 
     @Override
