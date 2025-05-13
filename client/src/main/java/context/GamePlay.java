01ceb595c72753c4173c562c964f995ac98803fd
mewilker <mewilk@byu.edu>
2025-05-13 14:21:51 -0600

    Stub functionality for options

--- client/src/main/java/context/GamePlay.java
+++ client/src/main/java/context/GamePlay.java
@@ -1,5 +1,6 @@
 package context;
 
+import chess.ChessGame;
 import facade.ServerFacade;
 import facade.WsMessageHandler;
 import ui.Display;
@@ -8,21 +9,56 @@ import websocket.messages.LoadGameMessage;
 import websocket.messages.NotificationMessage;
 
 public class GamePlay implements Context, WsMessageHandler {
-    ServerFacade facade;
+    ServerFacade server;
     Display display;
+    ChessGame.TeamColor color;
+    ChessGame game;
 
-    public GamePlay (ServerFacade facade, Display display){
+    public GamePlay(ServerFacade facade, Display display, ChessGame.TeamColor color) {
         this.display = display;
-        this.facade = facade;
+        this.server = facade;
+        this.color = color;
     }
+
     @Override
     public Context eval(String command) {
-        return null;
+        return switch (command) {
+            case "redraw", "r" -> redraw();
+            case "highlight", "h" -> highlight();
+            case "move", "m" -> move();
+            case "resign" -> resign();
+            case "leave", "l" -> leave();
+            default -> menu();
+        };
+    }
+
+    private Context redraw() {
+        return this;
+    }
+
+    private Context highlight() {
+        return this;
+    }
+
+    private Context move() {
+        return this;
+    }
+
+    private Context resign() {
+        return this;
+    }
+
+    private Context leave() {
+        return new PostLogin(server, display);
+    }
+
+    private Context menu() {
+        return this;
     }
 
     @Override
     public void handleError(ErrorMessage error) {
-
+        display.printError(error.getErrorMessage());
     }
 
     @Override
