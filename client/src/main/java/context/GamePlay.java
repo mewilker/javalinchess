4faacacb736ea35496799ca1e7410de4b4856ca3
mewilker <mewilk@byu.edu>
2025-09-16 16:06:35 -0600

    remove testme comment

--- client/src/main/java/context/GamePlay.java
+++ client/src/main/java/context/GamePlay.java
@@ -47,7 +47,6 @@ public class GamePlay implements Context, WsMessageHandler {
     }
 
     private Context highlight() {
-        //TODO: testme
         ChessPosition highlight = new ChessPosition(display.stringField("Where to highlight? (format: a1)"));
         display.printGame(color, game, highlight);
         return this;
