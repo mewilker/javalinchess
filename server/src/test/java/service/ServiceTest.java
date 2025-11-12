7e653907afbb78fca905702111e169507c9fb61d
mewilker <mewilk@byu.edu>
2025-11-11 18:21:00 -0700

    many passing unit tests

--- server/src/test/java/service/ServiceTest.java
+++ server/src/test/java/service/ServiceTest.java
@@ -2,6 +2,10 @@ package service;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.IntStream;
 
 public class ServiceTest {
 
@@ -10,8 +14,9 @@ public class ServiceTest {
         Assertions.assertTrue(false);
     }
 
-    @Test
-    public void passMe(){
+    @ParameterizedTest
+    @ValueSource (ints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13})
+    public void passMe(int num){
         Assertions.assertTrue(true);
     }
 }
