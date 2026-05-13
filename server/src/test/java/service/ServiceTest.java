e1c0a3f55d0bdb9dece09509345950c65c17a341
mewilker <mewilk@byu.edu>
2026-05-13 17:03:54 -0600

    begin porting tests

--- server/src/test/java/service/ServiceTest.java
+++ server/src/test/java/service/ServiceTest.java
@@ -1,22 +1,77 @@
 package service;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import com.mysql.cj.log.Log;
+import dataaccess.*;
+import datamodels.GameData;
+import org.junit.jupiter.api.*;
+import org.mindrot.jbcrypt.BCrypt;
+import response.*;
+import requests.*;
+import services.*;
+import datamodels.*;
 
-import java.util.stream.IntStream;
+import java.util.Collection;
 
 public class ServiceTest {
 
-    @Test
-    public void failMe(){
-        Assertions.assertTrue(false);
+    private static AuthDAO authdao;
+    private static UserDAO userdao;
+    private static GameDAO gamedao;
+
+    private String token;
+    private GameData game;
+
+    @BeforeAll
+    public static void dbInit() throws DataAccessException{
+        authdao = new MemAuthDAO();
+        userdao = new MemUserDAO();
+        gamedao = new MemGameDAO();
     }
 
-    @ParameterizedTest
-    @ValueSource (ints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13})
-    public void passMe(int num){
-        Assertions.assertTrue(true);
+    @BeforeEach
+    public void setup() throws DataAccessException{
+        gamedao.clear();
+        authdao.clear();
+        userdao.clear();
+        userdao.insertUser(new UserData("username", "password", "email"));
+        token = authdao.insertAuth("username").authToken();
+        userdao.insertUser(new UserData("me", "password", "email"));
+        authdao.insertAuth("me");
+        userdao.insertUser(new UserData("C3PO", BCrypt.hashpw("droid", BCrypt.gensalt()),"Threepio@jeditemple.com"));
+        gamedao.insertGame("gamegame");
+        gamedao.insertGame("game");
+        gamedao.insertGame("Sabaac");
+
+    }
+
+    @Test
+    @DisplayName("Positive Clear Test")
+    public void clearTest() throws DataAccessException{
+        ClearService service = new ClearService(userdao, authdao, gamedao);
+        Assertions.assertDoesNotThrow(service::clear);
+    }
+
+    @Test
+    @DisplayName("Register")
+    public void registerNew() throws DataAccessException {
+        RegisterService service = new RegisterService(userdao, authdao);
+        LoginResult result = (LoginResult) service.register(new RegisterRequest(
+                "R2-D2",
+                "skywalkers",
+                "Artoo@jeditemple.com"
+        ));
+        Assertions.assertNotNull(result.getAuthToken());
+        Assertions.assertEquals("R2-D2", result.getUsername());
+        Assertions.assertEquals(authdao.getAuth(result.getAuthToken()).username(), result.getUsername());
+    }
+
+    @Test
+    @DisplayName ("Login")
+    public void login() throws DataAccessException{
+        LoginService service = new LoginService(userdao, authdao);
+        LoginResult result = (LoginResult) service.login(new LoginRequest("C3PO","droid"));
+        Assertions.assertNotNull(result.getAuthToken());
+        Assertions.assertEquals("C3PO", result.getUsername());
+        Assertions.assertEquals(authdao.getAuth(result.getAuthToken()).username(), result.getUsername());
     }
 }
