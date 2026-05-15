7f35df664f2efa1eb5fdb091ffd79685c53bbc9b
mewilker <mewilk@byu.edu>
2026-05-14 18:53:30 -0600

    add negative serverfacade tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -34,12 +34,19 @@ public class ServerFacadeTests {
         http.doDelete(url + "/db", "");
     }
 
-    //clear b4 each test
     @Test
     @DisplayName("Positive Register")
     public void goodRegister() throws ServerErrorException {
         facade.register("Artoo", "anakin", "r2d2@jeditemple.com");
         Assertions.assertNotNull(facade.listGames()); // valid token received
+    }
+
+    @Test
+    @DisplayName("Negative Register")
+    public void badRegister() throws ServerErrorException{
+        goodRegister();
+        Assertions.assertThrows(ServerErrorException.class,
+                ()-> facade.register("Artoo", "r2", "r2"));
     }
 
     @Test
@@ -51,10 +58,24 @@ public class ServerFacadeTests {
     }
 
     @Test
+    @DisplayName("Negative Login")
+    public void badLogin()throws ServerErrorException{
+        goodRegister();
+        Assertions.assertThrows(ServerErrorException.class, ()-> facade.login("Artoo", "skywalker"));
+
+    }
+
+    @Test
     @DisplayName("Positive Logout")
     public void goodLogout()throws ServerErrorException{
         facade.register("Artoo", "anakin", "r2d2@jeditemple.com");
         Assertions.assertDoesNotThrow(()->facade.logout());
+    }
+
+    @Test
+    @DisplayName("Negative Logout")
+    public void badLogout(){
+        Assertions.assertThrows(ServerErrorException.class,()->facade.logout());
     }
 
     @Test
@@ -63,6 +84,12 @@ public class ServerFacadeTests {
         facade.register("Artoo", "anakin", "r2d2@jeditemple.com");
         int id = facade.createGame("sabaac");
         Assertions.assertTrue(id > 0);
+    }
+
+    @Test
+    @DisplayName("Negative Create Game")
+    public void badCreate(){
+        Assertions.assertThrows(ServerErrorException.class,()->facade.createGame("sabaac"));
     }
 
     @Test
@@ -76,11 +103,27 @@ public class ServerFacadeTests {
     }
 
     @Test
+    @DisplayName("Negative List Game")
+    public void badList(){
+        Assertions.assertThrows(ServerErrorException.class,()->facade.listGames());
+    }
+
+    @Test
     @DisplayName("Positive Join Game")
     public void goodJoin() throws ServerErrorException{
         facade.register("Artoo", "anakin", "r2d2@jeditemple.com");
         int id = facade.createGame("sabaac");
         Assertions.assertDoesNotThrow(()->facade.joinGame(ChessGame.TeamColor.BLACK, id));
+    }
+
+    @Test
+    @DisplayName("Negative Join Game")
+    public void badJoin() throws ServerErrorException{
+        facade.register("Artoo", "anakin", "r2d2@jeditemple.com");
+        int id = facade.createGame("sabaac");
+        facade.joinGame(ChessGame.TeamColor.BLACK, id);
+        facade.register("Vader", "Darth", "sith@empire.com");
+        Assertions.assertThrows(ServerErrorException.class, ()->facade.joinGame(ChessGame.TeamColor.BLACK,id));
     }
 
     @AfterAll
