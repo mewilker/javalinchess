59b2f66b2f925453f8d1c9cf4a9d45f5113d9c77
mewilker <mewilk@byu.edu>
2026-05-15 10:00:32 -0600

    port negative userdao unit tests

--- server/src/test/java/dataaccess/UserDataAccessTest.java
+++ server/src/test/java/dataaccess/UserDataAccessTest.java
@@ -35,10 +35,22 @@ public class UserDataAccessTest {
     }
 
     @Test
+    public void failInsert() {
+        Assertions.assertThrows(DataAccessException.class,
+                ()-> udao.insertUser(new UserData(null, null,null)));
+    }
+
+    @Test
     @DisplayName("Positive Get")
     public void get() throws DataAccessException{
         UserData threepio = udao.getUser(user.username());
         Assertions.assertEquals(user, threepio);
+    }
+
+    @Test
+    public void failGet() throws DataAccessException {
+        var got = udao.getUser("DoesNotExist");
+        Assertions.assertNull(got);
     }
 
 }
