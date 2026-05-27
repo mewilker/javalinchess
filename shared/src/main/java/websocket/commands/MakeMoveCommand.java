b653b6efe627ca494a7c65747855ceab5e31c012
mewilker <mewilk@byu.edu>
2026-05-26 19:10:05 -0600

    this was ridiculously difficult to get intellij to realize I was using java 25

--- shared/src/main/java/websocket/commands/MakeMoveCommand.java
+++ shared/src/main/java/websocket/commands/MakeMoveCommand.java
@@ -6,6 +6,7 @@ public class MakeMoveCommand extends UserGameCommand {
     private final ChessMove move;
 
     public MakeMoveCommand(String authToken, Integer gameID, ChessMove move) {
+        int coolNewJavaFeature = 1;
         super(CommandType.MAKE_MOVE, authToken, gameID);
         this.move = move;
     }
