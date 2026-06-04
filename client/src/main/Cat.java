20144cdebbfdea4d1a5c98f9e5d0a0735fe36a1f
mewilker <mewilk@byu.edu>
2026-06-04 16:18:24 -0600

    change Animal to Cat

---
+++ client/src/main/Cat.java
@@ -0,0 +1,5 @@
+public class Cat extends Animal {
+    public Cat(){
+        System.out.println("Animal is not defined so while this is valid code it should compile but not pass the quality checker");
+    }
+}
